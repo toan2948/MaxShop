b05e461e51ca9ca3898a98de90216480c2905f8a
toan0176 <xuantoan2948@gmail.com>
2021-02-16 13:19:10 +0100

    create API for products

--- MaxShop/backend/server.js
+++ MaxShop/backend/server.js
@@ -6,6 +6,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import products from './data/products.js' // the name without the extension '.js' will not work
+import productRouter from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -15,12 +16,7 @@ const app = express()
 
 app.get('/', (req, res) => res.send('API is running'))
 
-app.get('/api/products', (req, res) => res.json(products))
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(product => product._id === req.params.id)
-    res.json(product)
-})
+app.use('/api/products',productRouter)
 
 const PORT = process.env.PORT || 5000
 app.listen(5000, console.log(`server running on ${PORT}`))
